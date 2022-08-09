08459760a4d85a2e64e86988b7ca66e040a61b78
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-08-09 12:38:45 -0300

    adiciona requisitos 3 e 4

--- script.js
+++ script.js
@@ -12,6 +12,25 @@ const input = document.createElement('input');
 input.id = 'carta-texto';
 main.appendChild(input);
 
+const button = document.createElement('button');
+button.id = 'criar-carta';
+button.innerText = 'Criar carta';
+main.appendChild(button);
+
 const p = document.createElement('p');
 p.id = 'carta-gerada';
 main.appendChild(p);
+
+function spam() {
+  const array = input.value.split(' ');
+  const word = [];
+  for (let index = 0; index < array.length; index += 1) {
+    word.push(`<span>${array[index]}</span>`);
+  }
+  return word.join(' ');
+}
+
+function createLetter() {
+  p.innerHTML = spam();
+}
+button.addEventListener('click', createLetter);
